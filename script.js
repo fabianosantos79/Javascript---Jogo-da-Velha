25bcfe7c758b42b91feb08cd503e3e724d20e58b
Fabiano <fabiano.ssantos79@gmail.com>
2022-11-16 01:13:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let playing = false;
 
 // Events
 document.querySelector('.reset').addEventListener('click', reset);
-document.querySelector('.item').forEach(item => {
+document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('click', itemClick);
 });
 
@@ -22,8 +22,25 @@ document.querySelector('.item').forEach(item => {
 // Functions
 
 function itemClick(event) {
-    console.log(event.target);
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === ''){
+        square[item] = player;
+        renderSquare();
+        tooglePlayer();
+    }
 }
+
+
+
+function tooglePlayer() {
+    if(player === 'x'){
+        player = 'o';
+    }else{
+        player = 'x';
+    }
+    renderInfo();
+}
+
 
 
 function reset() {
@@ -41,12 +58,76 @@ function reset() {
     renderInfo();
 }
 
+
+
 function renderSquare() {
     for(let i in square){
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+
+    checkGame();
 }
+
+
+
+function checkGame() {
+    if(checkWinnerFor('x')){
+        warning = 'o "x" venceu';
+        playing = false;
+    }else if(checkWinnerFor('o')){
+        warning = 'O "o" venceu';
+        playing = false;
+    }else if(isFull()){
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+
+function checkWinnerFor(player) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos){
+        let pArray = pos[w].split(','); //a1,a2,a3
+        let hasWon = pArray.every((option) => {
+            if(square[option] === player){
+                return true;
+            }else{
+                return false;
+            }
+        });
+
+        if(hasWon){
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+
+function isFull() {
+    for(let i in square){
+        if(square[i] === ''){
+            return false;
+        }
+    }
+    return true;
+}
+
 
 function renderInfo() {
     document.querySelector('.vez').innerHTML = player;
